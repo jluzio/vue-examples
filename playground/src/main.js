979455341fa381922bafb3c54d5c04ac600c3bef
João Luzio <joao.luzio@gmail.com>
2019-01-23 12:46:25 +0000

    Plugin update

--- playground/src/main.js
+++ playground/src/main.js
@@ -8,14 +8,13 @@ import './plugins/jquery'
 import './plugins/jquery-datepicker'
 import './plugins/moment'
 import './plugins/toasted'
-import './plugins/vee-validation'
+import './plugins/vee-validate'
 import './plugins/vuelidate'
 import 'jquery-ui-themes/themes/base/jquery-ui.css'
 import 'jquery-ui-themes/themes/base/jquery-ui.min.css'
 import './plugins/directives'
 import './plugins/filters'
 import './plugins/app-config'
-import i18n from './plugins/i18n'
 
 import './config/app-config-core-config'
 
@@ -24,6 +23,7 @@ import './global-components'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './i18n'
 import './config/app-config-full-config'
 
 Vue.config.productionTip = false

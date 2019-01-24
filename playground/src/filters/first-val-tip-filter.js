bc7bfc745eb79c2091077de0b1df95047e6ce1f3
João Luzio <joao.luzio@gmail.com>
2019-01-24 13:21:15 +0000

    filters

---
+++ playground/src/filters/first-val-tip-filter.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import { Validation } from 'vuelidate'
+import validationUtil from '@/utils/vuelidate-util'
+
+/**
+ * @param {Validation} validation
+ * @param {String} nested
+ */
+export default function (validation, nested = false) {
+  return validationUtil.validationsMessages(validation, nested).shift()
+}
--- playground/src/plugins/filters.js
+++ playground/src/plugins/filters.js
@@ -3,6 +3,7 @@ import UppercaseFilter from '@/filters/uppercase-filter'
 import SubstringFilter from '@/filters/substring-filter'
 import QuoteFilter from '@/filters/quote-filter'
 import ValTipFilter from '@/filters/val-tip-filter'
+import FirstValTipFilter from '@/filters/first-val-tip-filter'
 import ValClassesFilter from '@/filters/val-classes-filter'
 
 const Plugin = {
@@ -12,6 +13,7 @@ const Plugin = {
     Vue.filter('substr', SubstringFilter)
     Vue.filter('quote', QuoteFilter)
     Vue.filter('valTip', ValTipFilter)
+    Vue.filter('firstValTip', FirstValTipFilter)
     Vue.filter('valClasses', ValClassesFilter)
   }
 }

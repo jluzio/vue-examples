f0f9bd4a2aa4a4b1ad9daa5243d07484933e0f68
João Luzio <joao.luzio@gmail.com>
2019-01-24 12:11:08 +0000

    cfg

--- playground/src/config/app-config-full-config.js
+++ playground/src/config/app-config-full-config.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-unused-vars */
 import Vue from 'vue'
 import $appCfg from '@/$appCfg'
-import './app-config-core-config'
 import store from '@/store'
 import services from '@/services'
 import i18n from '@/i18n'
+import './app-config-core-config'
 
 // BUG atm: assignments to imported instance $appCfg breaks IntelliSense
 // Workaround: local var

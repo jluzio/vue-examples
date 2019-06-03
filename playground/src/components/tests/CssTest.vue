d27aac86a0cc688de58bbea480ecc4d26721bcb2
João Luzio <joao.luzio@gmail.com>
2019-06-03 18:29:50 +0100

    css

--- playground/src/components/tests/CssTest.vue
+++ playground/src/components/tests/CssTest.vue
@@ -8,18 +8,42 @@
       <li class="list-group-item">
         <span class="text-scss">text-scss</span>
       </li>
+      <li class="list-group-item">
+        <button class="btn btn-primary" @click="includeColorClass = !includeColorClass">includeColorClass</button>
+        <button class="btn btn-primary" @click="includeBorderClass = !includeBorderClass">includeBorderClass</button>
+        <span class="cssTestBlock" :class="{colorClass: includeColorClass, borderClass: includeBorderClass}">Var Test 1</span>
+        <span class="cssTestBlock" :class="['colorClass', {borderClass: includeBorderClass}]">Var Test 2</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      includeColorClass: true,
+      includeBorderClass: false
+    }
+  }
 }
 </script>
 
 <style scoped>
 .text-css-scoped {
   color: aqua;
+}
+.cssTestBlock {
+  display: inline-block;
+  width: 100px;
+  height: 50px;
+  margin: 0 10px;
+}
+.colorClass {
+  background-color: red;
+}
+.borderClass {
+  border: 5px solid green;
 }
 </style>
 
